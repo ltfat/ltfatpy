a1b8ac7f84969c339a49cf70b826eb415ffce29d
allthatsounds <clara.hollomey@allthatsounds.com>
2023-12-11 01:30:22 +0100

    first raw version of ltfatpy-gabor

--- ltfatpy/__init__.py
+++ ltfatpy/__init__.py
@@ -1,12 +1,588 @@
-from oct2py.utils import Oct2PyError
-from .modules import Ltfat
+#Adapted from the oct2py project
 
+from oct2py.io import Cell, StructArray, read_file, write_file
+#from oct2py import Oct2Py
+
+import atexit
+import logging
+import os, sys
+import os.path as osp
+import shutil
+import tempfile
+import warnings
+
+from oct2py.dynamic import (
+    OctavePtr,
+    _make_function_ptr_instance,
+    _make_user_class,
+    _make_variable_ptr_instance,
+)
+
+import numpy as np
+from metakernel.pexpect import EOF, TIMEOUT  # type:ignore[import-untyped]
+from octave_kernel.kernel import STDIN_PROMPT, OctaveEngine  # type:ignore[import-untyped]
+
+bp = os.path.dirname(os.path.dirname(__file__))
+filepath = os.path.join(bp, 'ltfatpy')
+sys.path.append(filepath)
+import gabor as Gabor
+
+def add_methods_from(*modules):
+    def decorator(Class):
+        for module in modules:
+            for method in getattr(module, "__methods__"):
+                setattr(Class, method.__name__, method)
+        return Class
+    return decorator
+
+@add_methods_from(Gabor)
+class Ltfat():
+
+    """Manages an Octave session. based on Oct2Py
+
+    Uses MAT files to pass data between Octave and Numpy.
+    The function must either exist as an m-file in this directory or
+    on Octave's path.
+    The first command will take about 0.5s for Octave to load up.
+    The subsequent commands will be faster.
+
+    You may provide a logger object for logging events, or the oct2py.get_log()
+    default will be used.  When calling commands, logger.info() will be used
+    to stream output, unless a `stream_handler` is provided.
+
+    Parameters
+    ----------
+    logger : logging object, optional
+        Optional logger to use for Oct2Py session
+    timeout : float, optional
+        Timeout in seconds for commands
+    oned_as : {'row', 'column'}, optional
+        If 'column', write 1-D numpy arrays as column vectors.
+        If 'row', write 1-D numpy arrays as row vectors.}
+    temp_dir : str, optional
+        If specified, the session's MAT files will be created in the
+        directory, otherwise a default directory is used.  This can be
+        a shared memory (tmpfs) path.
+    convert_to_float : bool, optional
+        If true, convert integer types to float when passing to Octave.
+    backend: string, optional
+        The graphics_toolkit to use for plotting.
+    """
+
+    def __init__(  # noqa
+        self,
+        logger=None,
+        timeout=None,
+        oned_as="row",
+        temp_dir=None,
+        convert_to_float=True,
+        backend=None,
+    ):
+        """Start Octave and set up the session."""
+        self._oned_as = oned_as
+        self._engine = None
+        self._logger = None
+        self.logger = logger
+        self.timeout = timeout
+        self.backend = backend or "default"
+        if temp_dir is None:
+            temp_dir_obj = tempfile.mkdtemp()
+            self.temp_dir = temp_dir_obj
+            atexit.register(shutil.rmtree, self.temp_dir)
+        else:
+            self.temp_dir = temp_dir
+        self.convert_to_float = convert_to_float
+        self._user_classes = {}
+        self._function_ptrs = {}
+        self.restart()
+
+    @property
+    def logger(self):
+        """The logging instance used by the session."""
+        return self._logger
+
+    @logger.setter
+    def logger(self, value):
+        self._logger = value or get_log()
+        if self._engine:
+            self._engine.logger = self._logger
+
+    def _handle_stdin(self, line):
+        """Handle a stdin request from the session."""
+        return input(line.replace(STDIN_PROMPT, ""))
+
+    def __getattr__(self, attr):
+        
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        else: 
+            raise LtfatError("LTFATPY Error: Functionality not implemented in ltfatpy.")
+
+    def __enter__(self):
+        """Return octave object, restart session if necessary"""
+        if not self._engine:
+            self.restart()
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        """Close session"""
+        self.exit()
+
+    def __del__(self):
+        """Delete session"""
+        self.exit()
+
+    def exit(self):  # noqa
+        """Quits this octave session and cleans up."""
+        if self._engine:
+            self._engine.repl.terminate()
+        self._engine = None
+
+    def restart(self):
+        """Restart an Octave session in a clean state"""
+        if self._engine:
+            self._engine.repl.terminate()
+
+        if "OCTAVE_EXECUTABLE" not in os.environ and "OCTAVE" in os.environ:
+            os.environ["OCTAVE_EXECUTABLE"] = os.environ["OCTAVE"]
+
+        try:
+            self._engine = OctaveEngine(stdin_handler=self._handle_stdin, logger=self.logger)
+        except Exception as e:
+            raise LtfatError(str(e)) from None
+
+        # Add local Octave scripts.
+        bp = os.path.dirname(os.path.dirname(__file__))
+        filepath = os.path.join(bp, 'ltfatpy', 'octave', 'ltfat')
+        sys.path.append(filepath)
+        self._engine.eval('addpath("%s");' % filepath)
+        self._engine.eval("ltfatpystart")
+
+    def feval(self, func_path, *func_args, **kwargs):
+        """Run a function in Octave and return the result.
+
+        Parameters
+        ----------
+        func_path: str
+            Name of function to run or a path to an m-file.
+        func_args: object, optional
+            Args to send to the function.
+        nout: int or str, optional.
+            The desired number of returned values, defaults to 1. If nout
+            value is 'max_nout', _get_max_nout() will be used.
+        store_as: str, optional
+            If given, saves the result to the given Octave variable name
+            instead of returning it.
+        verbose : bool, optional
+            Log Octave output at INFO level.  If False, log at DEBUG level.
+        stream_handler: callable, optional
+            A function that is called for each line of output from the
+            evaluation.
+        timeout: float, optional
+            The timeout in seconds for the call.
+        plot_dir: str, optional
+            If specified, save the session's plot figures to the plot
+            directory instead of displaying the plot window.
+        plot_backend: str, optional
+            The plotting back end to use.
+        plot_name : str, optional
+            Saved plots will start with `plot_name` and
+            end with "_%%.xxx' where %% is the plot number and
+            xxx is the `plot_format`.
+        plot_format: str, optional
+            The format in which to save the plot.
+        plot_width: int, optional
+            The plot with in pixels.
+        plot_height: int, optional
+            The plot height in pixels.
+
+        Notes
+        -----
+        The function arguments passed follow Octave calling convention, not
+        Python. That is, all values must be passed as a comma separated list,
+        not using `x=foo` assignment.
+
+        Examples
+        --------
+        >>> from oct2py import octave
+        >>> cell = octave.feval('cell', 10, 10, 10)
+        >>> cell.shape
+        (10, 10, 10)
+
+        >>> from oct2py import octave
+        >>> x = octave.feval('linspace', 0, octave.pi() / 2)
+        >>> x.shape
+        (1, 100)
+
+        >>> from oct2py import octave
+        >>> x = octave.feval('svd', octave.hilb(3))
+        >>> x
+        array([[1.40831893],
+               [0.12232707],
+               [0.00268734]])
+        >>> # specify three return values
+        >>> (u, v, d) = octave.feval('svd', octave.hilb(3), nout=3)
+        >>> u.shape
+        (3, 3)
+
+        Returns
+        -------
+        The Python value(s) returned by the Octave function call.
+        """
+        if not self._engine:
+            msg = "Session is not open"
+            raise LtfatError(msg)
+
+        # nout handler
+        nout = kwargs.get("nout", None)
+        if nout is None:
+            nout = 1
+        elif nout == "max_nout":
+            nout = self._get_max_nout(func_path)
+
+        plot_dir = kwargs.get("plot_dir")
+
+        # Choose appropriate plot backend.
+        default_backend = "inline" if plot_dir else self.backend
+        backend = kwargs.get("plot_backend", default_backend)
+
+        settings = dict(
+            backend=backend,
+            format=kwargs.get("plot_format"),
+            name=kwargs.get("plot_name"),
+            width=kwargs.get("plot_width"),
+            height=kwargs.get("plot_height"),
+            resolution=kwargs.get("plot_res"),
+        )
+        self._engine.plot_settings = settings
+
+        dname = osp.dirname(func_path)
+        fname = osp.basename(func_path)
+        func_name, ext = osp.splitext(fname)
+        if ext and ext != ".m":
+            msg = "Need to give path to .m file"
+            raise TypeError(msg)
+
+        if func_name == "clear":
+            msg = 'Cannot use `clear` command directly, use eval("clear(var1, var2)")'
+            raise LtfatError(msg)
+
+        stream_handler = kwargs.get("stream_handler")
+        verbose = kwargs.get("verbose", True)
+        store_as = kwargs.get("store_as", "")
+        timeout = kwargs.get("timeout", self.timeout)
+        if not stream_handler:
+            stream_handler = self.logger.info if verbose else self.logger.debug
+
+        return self._feval(
+            func_name,
+            func_args,
+            dname=dname,
+            nout=nout,
+            timeout=timeout,
+            stream_handler=stream_handler,
+            store_as=store_as,
+            plot_dir=plot_dir,
+        )
+
+    def eval(  # noqa
+        self,
+        cmds,
+        verbose=True,
+        timeout=None,
+        stream_handler=None,
+        temp_dir=None,
+        plot_dir=None,
+        plot_name="plot",
+        plot_format="svg",
+        plot_backend=None,
+        plot_width=None,
+        plot_height=None,
+        plot_res=None,
+        nout=0,
+        **kwargs,
+    ):
+        """
+        Evaluate an Octave command or commands.
+
+        Parameters
+        ----------
+        cmds : str or list
+            Commands(s) to pass to Octave.
+        verbose : bool, optional
+             Log Octave output at INFO level.  If False, log at DEBUG level.
+        stream_handler: callable, optional
+            A function that is called for each line of output from the
+            evaluation.
+        timeout : float, optional
+            Time to wait for response from Octave (per line).  If not given,
+            the instance `timeout` is used.
+        nout : int or str, optional.
+            The desired number of returned values, defaults to 0.  If nout
+            is 0, the `ans` will be returned as the return value. If nout
+            value is 'max_nout', _get_max_nout() will be used.
+        temp_dir: str, optional
+            If specified, the session's MAT files will be created in the
+            directory, otherwise a the instance `temp_dir` is used.
+            a shared memory (tmpfs) path.
+        plot_dir: str, optional
+            If specified, save the session's plot figures to the plot
+            directory instead of displaying the plot window.
+        plot_name : str, optional
+            Saved plots will start with `plot_name` and
+            end with "_%%.xxx' where %% is the plot number and
+            xxx is the `plot_format`.
+        plot_format: str, optional
+            The format in which to save the plot (PNG by default).
+        plot_width: int, optional
+            The plot with in pixels.
+        plot_height: int, optional
+            The plot height in pixels.
+        plot_backend: str, optional
+            The plot backend to use.
+        plot_res: int, optional
+            The plot resolution in pixels per inch.
+        **kwargs Deprecated kwargs.
+
+        Examples
+        --------
+        >>> from oct2py import octave
+        >>> octave.eval('disp("hello")') # doctest: +SKIP
+        hello
+        >>> x = octave.eval('round(quad(@sin, 0, pi/2));')
+        >>> x
+        1.0
+
+        >>> a = octave.eval('disp("hello");1;')  # doctest: +SKIP
+        hello
+        >>> a = octave.eval('disp("hello");1;', verbose=False)
+        >>> a
+        1.0
+
+        >>> from oct2py import octave
+        >>> lines = []
+        >>> octave.eval('for i = 1:3; disp(i);end', \
+                        stream_handler=lines.append)
+        >>> lines  # doctest: +SKIP
+        [' 1', ' 2', ' 3']
+
+        Returns
+        -------
+        out : object
+            Octave "ans" variable, or None.
+
+        Notes
+        -----
+        The deprecated `log` kwarg will temporarily set the `logger` level to
+        `WARN`.  Using the `logger` settings directly is preferred.
+        The deprecated `return_both` kwarg will still work, but the preferred
+        method is to use the `stream_handler`.  If `stream_handler` is given,
+        the `return_both` kwarg will be honored but will give an empty string
+        as the response.
+
+        Raises
+        ------
+        LtfatError
+            If the command(s) fail.
+        """
+        if isinstance(cmds, str):
+            cmds = [cmds]
+
+        prev_temp_dir = self.temp_dir
+        self.temp_dir = temp_dir or self.temp_dir
+        prev_log_level = self.logger.level
+
+        if kwargs.get("log") is False:
+            self.logger.setLevel(logging.WARN)
+
+        for name in ["log", "return_both"]:
+            if name not in kwargs:
+                continue
+            msg = "Using deprecated `%s` kwarg, see docs on `Oct2Py.eval()`"
+            warnings.warn(msg % name, stacklevel=2)
+
+        return_both = kwargs.pop("return_both", False)
+        lines: list[str] = []
+        if return_both and not stream_handler:
+            stream_handler = lines.append
+
+        ans = None
+        for cmd in cmds:
+            resp = self.feval(
+                "evalin",
+                "base",
+                cmd,
+                nout=nout,
+                timeout=timeout,
+                stream_handler=stream_handler,
+                verbose=verbose,
+                plot_dir=plot_dir,
+                plot_name=plot_name,
+                plot_format=plot_format,
+                plot_backend=plot_backend,
+                plot_width=plot_width,
+                plot_height=plot_height,
+                plot_res=plot_res,
+            )
+            if resp is not None:
+                ans = resp
+
+        self.temp_dir = prev_temp_dir
+        self.logger.setLevel(prev_log_level)
+
+        if return_both:
+            return "\n".join(lines), ans
+        return ans
+
+    def _feval(  # noqa
+        self,
+        func_name,
+        func_args=(),
+        dname="",
+        nout=0,
+        timeout=None,
+        stream_handler=None,
+        store_as="",
+        plot_dir=None,
+    ):
+        """Run the given function with the given args."""
+        engine = self._engine
+        if engine is None:
+            msg = "Session is closed"
+            raise LtfatError(msg)
+
+        # Set up our mat file paths.
+        out_file = osp.join(self.temp_dir, "writer.mat")
+        out_file = out_file.replace(osp.sep, "/")
+        in_file = osp.join(self.temp_dir, "reader.mat")
+        in_file = in_file.replace(osp.sep, "/")
+
+        func_args = list(func_args)
+        ref_indices = []
+        for i, value in enumerate(func_args):
+            if isinstance(value, OctavePtr):
+                ref_indices.append(i + 1)
+                func_args[i] = value.address
+        ref_arr = np.array(ref_indices)
+
+        # Save the request data to the output file.
+        req = dict(
+            func_name=func_name,
+            func_args=tuple(func_args),
+            dname=dname or "",
+            nout=nout,
+            store_as=store_as or "",
+            ref_indices=ref_arr,
+        )
+
+        write_file(req, out_file, oned_as=self._oned_as, convert_to_float=self.convert_to_float)
+
+        # Set up the engine and evaluate the `_pyeval()` function.
+        engine.line_handler = stream_handler or self.logger.info
+        if timeout is None:
+            timeout = self.timeout
+
+        try:
+            engine.eval(f'_pyeval("{in_file}", "{out_file}");', timeout=timeout)
+        except KeyboardInterrupt:
+            stream_handler(engine.repl.interrupt())
+            raise
+        except TIMEOUT:
+            stream_handler(engine.repl.interrupt())
+            msg = "Timed out, interrupting"
+            raise LtfatError(msg) from None
+        except EOF:
+            if not self._engine:
+                return
+            stream_handler(engine.repl.child.before)
+            self.restart()
+            msg = "Session died, restarting"
+            raise LtfatError(msg) from None
+
+        # Read in the output.
+        resp = read_file(in_file, self)
+        if resp["err"]:
+            msg = self._parse_error(resp["err"])
+            raise LtfatError(msg)
+
+        result = resp["result"].ravel().tolist()
+        if isinstance(result, list) and len(result) == 1:
+            result = result[0]
+
+        # Check for sentinel value.
+        if (
+            isinstance(result, Cell)
+            and result.size == 1
+            and isinstance(result[0], str)
+            and result[0] == "__no_value__"
+        ):
+            result = None
+
+        if plot_dir:
+            engine.make_figures(plot_dir)
+
+        return result
+
+    def _parse_error(self, err):
+        """Create a traceback for an Octave evaluation error."""
+        self.logger.debug(err)
+        stack = err.get("stack", [])
+        if not err["message"].startswith("parse error:"):
+            err["message"] = "error: " + err["message"]
+        errmsg = "Octave evaluation error:\n%s" % err["message"]
+
+        if not isinstance(stack, StructArray):
+            return errmsg
+
+        errmsg += "\nerror: called from:"
+        for item in stack[:-1]:
+            errmsg += "\n    %(name)s at line %(line)d" % item
+            try:  # noqa
+                errmsg += ", column %(column)d" % item
+            except Exception:  # noqa
+                pass
+        return errmsg
+
+        #override help from oct2py because of formatting
+    def help(self, str):
+        self.eval('help '+str)
+
+def get_log(name=None):
+    """Return a console logger.
+
+    Output may be sent to the logger using the `debug`, `info`, `warning`,
+    `error` and `critical` methods.
+
+    Parameters
+    ----------
+    name : str
+        Name of the log.
+    """
+    name = "oct2py" if name is None else "oct2py." + name
+
+    log = logging.getLogger(name)
+    log.setLevel(logging.INFO)
+    return log
+
+
+
+class LtfatError(Exception):
+    """Called when we can't open Octave or Octave throws an error"""
+
+    pass
+
+#def add_functions_as_methods(functions):
+#    def decorator(Class):
+#        for function in functions:
+#            setattr(Class, function.__name__, function)
+#        return Class
+#    return decorator
 
 try:
     ltfat = Ltfat()
 #    ltfat.get_functions()
 #throw an error if we can not instantiate ltfat
-except Oct2PyError as e:
+except LtfatError as e:
     print(e)  # noqa
 
 
---
+++ ltfatpy/gabor.py
@@ -0,0 +1,220 @@
+#Code adapted from https://www.qtrac.eu/pyclassmulti.html .
+
+def register_method(methods):
+    def register_method(method):
+        methods.append(method)
+        return method # Unchanged
+    return register_method
+
+__methods__ = []
+register_method = register_method(__methods__)
+
+#Basic Time/Frequency analysis
+#   not implemented, to comply with future restructuring
+
+#Gabor systems
+@register_method
+def dgt(self, f, g, a, M, lt = [0, 1], phase = 'freqinv', nout = 1):
+    [c, L] = self.feval('dgt', f, g, a, M, lt, phase, nout)
+    if nout == 1:
+        return c
+    else:
+        return c, L
+
+@register_method
+def idgt(self, c, g, a, Ls = "", lt = [0, 1], nout = 1):
+    f = self.feval('idgt',c, g, a, Ls, lt, nout)
+    return f
+
+@register_method
+def isgram(self, s, g, a, Ls ="", nout = 1):
+    [f, relres, iter] = self.feval('isgram', s, g, a, Ls, nout = 1)
+
+@register_method
+def isgramreal(self, s, g, a, M, Ls ="", nout = 1):
+    [f, relres, iter] = self.feval('isgramreal', s, g, a, M, Ls, nout = 1)
+
+@register_method
+def dgt2(self, f, g, a, M, nout = 1):
+    #not implemented: overload with several window functions
+    [c, Ls] = self.feval('dgt2', f, g, a, M, nout = 1)
+
+@register_method
+def idgt2(self, c, g, a, nout = 1):
+    #not implemented: overload with several window functions
+    f = self.feval('idgt2', c, g, a, nout = 1)
+    return f
+
+@register_method
+def dgtreal(self, f, g, a, M, L ="", nout = 1):
+    [c, Ls] = self.feval('dgtreal', f, g, a, M, L, nout = 1)
+
+@register_method
+def idgtreal(self, c, g, a, M, Ls ="", nout = 1):
+    [f, relres, iter] = self.feval('idgtreal', c, g, a, M, Ls, nout = 1)
+
+@register_method
+def gabwin(self, g, a, M, L ="", nout = 1):
+    [g, info] = self.feval('gabwin', g, a, M, L, nout = 1)
+
+@register_method
+def projkern(self, cin, g, a, nout = 1):
+    #not implemented: overload with second window
+    cout = self.feval('projkern', cin, g, a, nout = 1)
+
+@register_method
+def dgtlength(self, Ls, a, M, lt = [0, 1], nout = 1):
+    L = self.feval('dgtlength', Ls, a, M, lt, nout = 1)
+    return L
+
+#Multi-Gabor systems
+@register_method
+def multidgtrealmp(self, f, dicts, errdb, maxit, nout = 1):
+    [c,frec,info] = self.feval('multidgtrealmp', f,dicts,errdb,maxit, nout = 3)
+
+#Wilson bases and WMDCT
+#   not implemented, to comply with future restructuring
+
+#Reconstructing windows
+@register_method
+def gabdual(self, g, a, M, lt, nout = 1):
+    gd = self.feval('gabdual', g, a, M,'lt',lt, nout)
+    return gd
+
+@register_method
+def gabtight(self, g, a, M, lt, nout = 1):
+    gt = self.feval('gabtight', g, a, M,'lt',lt, nout)
+    return gt
+
+@register_method
+def gabfirdual(self, Ldual, g, a, M, nout = 1):
+    gd = self.feval('gabfirdual', Ldual, g, a, M, nout)
+    return gd
+
+@register_method
+def gabfirtight(self, Ldual, g, a, M, nout = 1):
+    gt = self.feval('gabfirtight', Ldual, g, a, M, nout)
+    return gt
+
+@register_method
+def gabprojdual(self, g1, g2, a, M, nout = 1):
+    gamma = self.feval('gabprojdual', g1, g2, a, M, nout)
+    return gamma
+
+@register_method
+def gabmixdual(self, gm, g, a, M, L, nout = 1):
+    gd = self.feval('gabmixdual', gm, g, a, M, L, nout)
+    return gd
+
+
+#Conditions numbers
+@register_method
+def gabframebounds(self, g, a, M, nout=2):
+    [A, B] = self.feval('gabframebounds', g, a, M, nout = 2)
+    return A, B
+
+@register_method
+def gabrieszbounds(self, g, a, M, nout=2):
+    [A, B] = self.feval('gabrieszbounds', g, a, M, nout = 2)
+    return A, B
+
+@register_method
+def gabdualnorm(self, g, gamma, a, M, lt, nout = 1):
+    dn = self.feval('gabdualnorm', g, gamma, a, M, lt, nout)
+    return dn
+
+@register_method
+def gabframediag(self, g, a, M, L, lt, nout = 1):
+    d = self.feval('gabframediag', g, a, M, L, lt, nout)
+    return d
+
+
+#Phase gradient methods and reassignment
+@register_method
+def gabphasegrad(self, method, f, g, a, M, nout = 2):
+    [tgrad,fgrad,c] = self.feval('gabphasegrad', method, f, g, a, M, nout)
+
+@register_method
+def gabphasederiv(self, dflag, method, f, g, a, M, nout = 2):
+    [phased, c] = self.feval('gabphasederiv', dflag, method, f, g, a, M, nout)
+
+@register_method
+def gabphasederivreal(self, dflag, method, f, g, a, M, nout = 2):
+    [phased, c] = self.feval('gabphasederivreal', dflag, method, f, g, a, M, nout)
+
+@register_method
+def gabreassign(self, s, tgrad, fgrad, a, nout = 1):
+    sr = self.feval('gabreassign', s, tgrad, fgrad, a, nout)
+    return sr
+
+@register_method
+def gabreassignreal(self, s, tgrad, fgrad, a, M, nout = 1):
+    sr = self.feval('gabreassignreal', s, tgrad, fgrad, a, M, nout)
+    return sr
+
+@register_method
+def gabreassignadjust(self, s, pderivs, a, mu, nout = 1):
+    sr = self.feval('gabreassignadjust', s, pderivs, a, mu, nout)
+    return sr
+
+
+#Phase reconstruction
+@register_method
+def constructphase(self, s, g, a, tol, mask, usephase, nout = 1):
+    [c,newphase,usedmask,tgrad,fgrad] = self.feval('constructphase', s, g, a, tol, mask, usephase, nout)
+
+@register_method
+def constructphasereal(self, s, g, a, M, tol, mask, usephase, nout = 1):
+    [c,newphase,usedmask,tgrad,fgrad] = self.feval('constructphasereal', s, g, a, M, tol, mask, usephase, nout)
+
+
+#Phase conversions
+@register_method
+def phaselock(self, c, a, nout = 1):
+    c = self.feval('phaselock', c, a, nout)
+    return c
+
+@register_method
+def phaseunlock(self, c, a, nout = 1):
+    c = self.feval('phaseunlock', c, a, nout)
+    return c
+
+@register_method
+def phaselockreal(self, c, a, M, nout = 1):
+    c = self.feval('phaselock', c, a, M, nout)
+    return c
+
+@register_method
+def phaseunlockreal(self, c, a, M, nout = 1):
+    c = self.feval('phaseunlock', c, a, M, nout)
+    return c
+
+@register_method
+def symphase(self, c, a, nout = 1):
+    c = self.feval('symphase', c, a, nout)
+    return c
+
+
+#Support for non-separable lattices
+@register_method
+def matrix2latticetype(self, L, V, nout = 1):
+    [a,M,lt] = self.feval('matrix2latticetype', L, V, nout = 3)
+
+@register_method
+def latticetype2matrix(self, L, a, M, lt, nout = 1):
+    V = self.feval('latticetype2matrix', L, a, M, lt, nout = 1)
+    return V
+
+@register_method
+def shearfind(self, L, a, M, lt, nout = 1):
+    [s0,s1,br] = self.feval('shearfind', L,a,M,lt, nout)
+
+@register_method
+def noshearlength(Ls, a, M, lt, nout):
+    L = self.feval('noshearlength', Ls,a,M,lt, nout)
+    return L
+
+
+#Plots
+    #not yet implemented
+
